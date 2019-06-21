e66bfd1d387f003f41c72c9f0b3ebb55ca5732b2
Afik Bar <afikbar@gmail.com>
2019-06-21 20:44:25 +0300

    [final]  add recv_from to LSP & cleanup

--- final/router.py
+++ final/router.py
@@ -22,12 +22,13 @@ class LSP(object):
         self.src = int(src)
         self.seq = int(seq)
         self.neighbors = neighbors
+        self.recv_from = self.src
 
     def get_weight(self, neigh) -> int:
         return self.neighbors[neigh]
 
     def to_message(self) -> str:
-        lsp = "LSP;{};{}".format(self.src, self.seq)
+        lsp = "LSP;{};{};{}".format(self.recv_from, self.src, self.seq)
         for neigh, weight in self.neighbors.items():
             lsp += ";{};{}".format(neigh, weight)
 
@@ -47,11 +48,13 @@ class LSP(object):
 
     @staticmethod
     def parse_lsp(data):
-        # expected data is: LSP;[src];[sequence];[neighbor name];[edge weight]
+        # expected data is: LSP;[recv_from];[src];[sequence];[neighbor name];[edge weight]
         split_data = data.split(sep=';')[1:]
-        src, seq, neighs = split_data[0], split_data[1], split_data[2:]
+        recv_from, src, seq, neighs = split_data[0], split_data[1], split_data[2], split_data[3:]
         neighs_dict = {int(neighs[i]): int(neighs[i + 1]) for i in range(0, len(neighs), 2)}
-        return LSP(src, seq, neighs_dict)
+        lsp = LSP(src, seq, neighs_dict)
+        lsp.recv_from = recv_from
+        return lsp
 
 
 # endregion
@@ -111,9 +114,9 @@ class Router(object):
         self.listen = False
         self.table_lock = Lock()
         self.udp_output_lock = Lock()
-        self.udp_output_handler = open("UDP_output_router_{}.txt".format(self.name), 'a')
+        self.udp_output_handler = open("UDP_output_router_{}.txt".format(self.name), 'w')
         self.tcp_output_lock = Lock()
-        self.tcp_output_handler = open("TCP_output_router_{}.txt".format(self.name), 'a')
+        self.tcp_output_handler = open("TCP_output_router_{}.txt".format(self.name), 'w')
         self.routing_table = {}  # {dest: {cost: , next: }}
         self.round = 1
         self.lsdb_lock = Lock()
@@ -166,11 +169,8 @@ class Router(object):
         with socket(AF_INET, SOCK_DGRAM) as s:
             s.bind((self.ip, self.udp_port))
             while self.is_listen():
-                # while True:
                 data, (c_ip, c_port) = s.recvfrom(4096)
-                # if not data:
-                #     break
-                print("\n UDP Connection on Router {}, From {}:{}".format(self.name, c_ip, c_port))
+                # print("\n UDP Connection on Router {}, From {}:{}".format(self.name, c_ip, c_port))
                 action = Thread(target=self.handle_message, args=(data, c_ip, c_port))
                 action.start()
 
@@ -181,53 +181,38 @@ class Router(object):
             while self.is_listen():
                 conn, (c_ip, c_port) = s.accept()
                 with conn:
-                    print("\n TCP Connection on Router {}, From {}:{}".format(self.name, c_ip, c_port))
-                    # while True:
+                    # print("\n TCP Connection on Router {}, From {}:{}".format(self.name, c_ip, c_port))
                     data = conn.recv(4096)
-                    # if not data:
-                    #     break
-                    # conn.sendall(data)
-                    # self.handle_message(data)
                     action = Thread(target=self.handle_message, args=(data, c_ip, c_port))
                     action.start()
 
     def handle_message(self, data: bytes, c_ip, c_port):
         message = data.decode()
         if message == 'PRINT-ROUTING-TABLE':
-            # DO PRINT
             self.write_routing_table()
         elif message == 'SHUT-DOWN':
             self.stop_listeners()
         elif message == 'UPDATE-ROUTING-TABLE':
-            # do LINKSTATE
-            # self.round += 1
-            # do Update routing table
-            print("\n Update Routing Table in router {}".format(self.name))
+            # print("\n Update Routing Table in router {}".format(self.name))
             self.update_routing_table(c_ip, c_port)
         elif message.startswith('ROUTE'):
-            print("\n Route in router {}, message: {} ".format(self.name, message))
+            # print("\n Route in router {}, message: {} ".format(self.name, message))
             self.udp_route(message)
         elif message.startswith('LSP'):
-            self.receive_lsp(message, c_ip, c_port)
+            self.receive_lsp(message)
 
     def write_routing_table(self):
         with self.table_lock:
             lines = ["{};{}".format(*row.values()) for row in self.routing_table.values()]
-            print("\n New routing table for router {}:".format(self.name))
-            print('    \n'.join(lines) + '\n')
-        # filename_template = "UDP_output_router_{}.txt"
 
         with self.udp_output_lock:
-            # with self.udp_output_handler as f:
             self.udp_output_handler.write('\n'.join(lines) + '\n')
             # f.flush()
 
     def udp_route(self, data):
         _, dest, message = data.split(sep=';')  # message doesn't contain ';'
-        # filename = "UDP_output_router_{}.txt".format(self.name)
-        print("Writing route to file: " + data)
+        # print("Writing route to file: " + data)
         with self.udp_output_lock:
-            # with open(filename, 'a') as f:
             self.udp_output_handler.write(data + '\n')
             # f.flush()
 
@@ -260,20 +245,18 @@ class Router(object):
                 print("\nError on Router {} - {}. Tried to send to router {}:{}".format(self.name, e, ip, port))
 
     def tcp_flood(self, node: Node, lsp: str):
-        # filename = "TCP_output_router_{}.txt".format(self.name)
         message = "UPDATE;{};{}".format(self.name, node.name)
         with self.tcp_output_lock:
-            # with open(filename, 'a') as f:
             self.tcp_output_handler.write(message + '\n')
             # f.flush()
         self.tcp_send(node.ip, node.tcp_port, lsp)
 
-    def flood_lsp(self, lsp: str, skip_ip=None, skip_port=None):
-        # expected data is: LSP;[src];[sequence];[neighbor name];[edge weight]
+    def flood_lsp(self, lsp: str, src=None, recv_from=None):
+        # expected data is: LSP;[recv_from];[src];[sequence];[neighbor name];[edge weight]
         threads = []
         with self.neighbors_lock:
             for neigh, node in self.neighbors.items():
-                if node.ip == skip_ip and node.udp_port == skip_port:
+                if neigh in [recv_from, src]:
                     continue  # Dont send lsp to the node you received it from
                 action = Thread(target=self.tcp_flood, args=(node, lsp))
                 action.start()
@@ -281,7 +264,7 @@ class Router(object):
         for action in threads:
             action.join()
 
-    def receive_lsp(self, data, c_ip, c_port):
+    def receive_lsp(self, data):
         # expected data is: LSP;[src];[sequence];[neighbor name];[edge weight]
         lsp = LSP.parse_lsp(data)
         with self.lsdb_lock:
@@ -290,12 +273,12 @@ class Router(object):
         # forward to all
         # TODO: check if lsp should be flooded?
         if updated:
-            self.flood_lsp(data)
+            self.flood_lsp(data, src=lsp.src, recv_from=lsp.recv_from)
 
     def update_routing_table(self, c_ip, c_port):
         with self.lsdb_lock:
-            lsp = self.lsdb[self.name].to_message()
-        self.flood_lsp(lsp)
+            lsp = self.lsdb[self.name]
+        self.flood_lsp(lsp.to_message(), src=lsp.src, recv_from=lsp.recv_from)
         # Wait until lsp is recieved from all
         while True:
             with self.lsdb_lock:
@@ -307,14 +290,8 @@ class Router(object):
                 if dest == self.name:
                     continue
                 with self.lsdb_lock:
-                    new_next, new_dist = dijkstra3(self.lsdb.db, self.name, dest)
+                    new_next, new_dist = dijkstra(self.lsdb.db, self.name, dest)
                     self.routing_table[dest] = table_entry(new_dist, new_next)
-                    print(
-                        "\n Updated routing table in round {} from {} to {} with cost: {}, next: {}".format(self.round,
-                                                                                                            self.name,
-                                                                                                            dest,
-                                                                                                            new_dist,
-                                                                                                            new_next))
 
         # Update routing table
 
@@ -363,24 +340,11 @@ def table_entry(cost, next_hop):
     return {'cost': cost, 'next': next_hop}
 
 
-def dijkstra3(db, src, dest=None):
-    """ Implement Dijkstra's algorithm
-    : param db: Dictionary with vertices of graph as keys and the
-        corresponding value being a dictionary with neighboring
-        vertices as keys and distance to them from the original vertex as
-        values.
-    : param src: Source vertex
-    : param dest: Destination vertex
-    : returns D: A dictionary with vertices as keys and corresponding value
-        is a list with the first element being a list of the shortest
-        path to the vertex from src and the second element being the shortest
-        distance to that vertex
-    """
-    # extract vertices from db and mark them all as unvisited
+def dijkstra(db, src, dest=None):
+    # vertices from db as unvisited
     unvisited = list(db.keys())
     # Initialize dictionary of shortest distances
     D = {v: {'path': [], 'distance': 2 ** 31 - 1} for v in unvisited}
-    # Zero out distance to start vertex
     D[src]['distance'] = 0
     # While all vertices haven't been yet visited
     while len(unvisited) > 0:
@@ -390,9 +354,6 @@ def dijkstra3(db, src, dest=None):
         D[curr]['path'].append(curr)
         # We're at the destination already
         if curr == dest:
-            print("\n Found shortest path from {} to {}, length of: {}, \n    path: {}".format(src, dest,
-                                                                                               D[dest]['distance'],
-                                                                                               D[dest]['path']))
             return D[dest]['path'][1], D[dest]['distance']
         # Loop over all neighboring vertices
         for neigh, dist in db[curr].neighbors.items():
@@ -411,121 +372,3 @@ def dijkstra3(db, src, dest=None):
         # Remove current node from unvisited ones
         unvisited.remove(curr)
     return D
-
-
-def dijkstra(lsdb: LSDB, src, dest, visited=[], distances={}, predecessors={}):
-    """ calculates a shortest path tree routed in src
-    """
-    # a few sanity checks
-    # if src not in lsdb:
-    #     raise TypeError('The root of the shortest path tree cannot be found')
-    # if dest not in lsdb:
-    #     raise TypeError('The target of the shortest path cannot be found')
-    # stop condition
-    if src == dest:
-        # We build the shortest path and display it
-        path = []
-        pred = dest
-        while pred:
-            path.append(pred)
-            pred = predecessors.get(pred, None)
-        return distances[dest], path[-2]
-        # print('shortest path: ' + str(path) + " cost=" + str(distances[dest]))
-    else:
-        # if it is the initial  run, initializes the cost
-        if not visited:
-            distances[src] = 0
-        # visit the neighbors
-        for neigh, weight in lsdb[src].neighbors.items():
-            if neigh not in visited:
-                new_distance = distances[src] + weight
-                if new_distance < distances.get(neigh, 2 ** 31 - 1):
-                    distances[neigh] = new_distance
-                    predecessors[neigh] = src
-        # mark as visited
-        visited.append(src)
-        # now that all neighbors have been visited: recurse
-        # select the non visited node with lowest distance 'x'
-        # run Dijskstra with src='x'
-        unvisited = {}
-        for k in lsdb:
-            if k not in visited:
-                unvisited[k] = distances.get(k, 2 ** 31 - 1)
-        if unvisited:
-            x = min(unvisited, key=unvisited.get)
-        else:
-            x = dest
-        return dijkstra(lsdb, x, dest, visited, distances, predecessors)
-
-
-def dijkstra2(G, start, end=None):
-    """
-    Find shortest paths from the start vertex to all
-    vertices nearer than or equal to the end.
-    The input graph G is assumed to have the following
-    representation: A vertex can be any object that can
-    be used as an index into a dictionary.  G is a
-    dictionary, indexed by vertices.  For any vertex v,
-    G[v] is itself a dictionary, indexed by the neighbors
-    of v.  For any edge v->w, G[v][w] is the length of
-    the edge.  This is related to the representation in
-    <http://www.python.org/doc/essays/graphs.html>
-    where Guido van Rossum suggests representing graphs
-    as dictionaries mapping vertices to lists of neighbors,
-    however dictionaries of edges have many advantages
-    over lists: they can store extra information (here,
-    the lengths), they support fast existence tests,
-    and they allow easy modification of the graph by edge
-    insertion and removal.  Such modifications are not
-    needed here but are important in other graph algorithms.
-    Since dictionaries obey iterator protocol, a graph
-    represented as described here could be handed without
-    modification to an algorithm using Guido's representation.
-    Of course, G and G[v] need not be Python dict objects;
-    they can be any other object that obeys dict protocol,
-    for instance a wrapper in which vertices are URLs
-    and a call to G[v] loads the web page and finds its links.
-
-    The output is a pair (D,P) where D[v] is the distance
-    from start to v and P[v] is the predecessor of v along
-    the shortest path from s to v.
-
-    Dijkstra's algorithm is only guaranteed to work correctly
-    when all edge lengths are positive. This code does not
-    verify this property for all edges (only the edges seen
-     before the end vertex is reached), but will correctly
-    compute shortest paths even for some graphs with negative
-    edges, and will raise an exception if it discovers that
-    a negative edge has caused it to make a mistake.
-    """
-
-    D = {}  # dictionary of final distances
-    P = {}  # dictionary of predecessors
-    Q = priorityDictionary()  # est.dist. of non-final vert.
-    Q[start] = 0
-
-    for v in Q:
-        D[v] = Q[v]
-        if v == end: break
-
-        for w in G[v]:
-            vwLength = D[v] + G[v][w]
-            if w in D:
-                if vwLength < D[w]:
-                    raise ValueError("Dijkstra: found better path to already-final vertex")
-            elif w not in Q or vwLength < Q[w]:
-                Q[w] = vwLength
-                P[w] = v
-    Path = []
-    while True:
-        Path.append(end)
-        if end == start: break
-        end = P[end]
-    Path.reverse()
-    return D[end], Path[1]
-
-# endregion
-
-
-# Priority dictionary using binary heaps
-# David Eppstein, UC Irvine, 8 Mar 2002
